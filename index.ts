9b79889c642113d081d3b34a822ae848fed278e3
Jeffrey Rose <jtr219@outlook.com>
2020-12-22 15:33:24 -0500

    first commit

---
+++ index.ts
@@ -0,0 +1,30 @@
+const express = require("express")
+
+const app = express()
+
+const port = process.env.PORT || 3030
+
+// home page
+app.get("/", (req, res) => {
+    res.type("text/plain")
+    res.send("beep boop")
+})
+
+// 404 page
+app.use((req, res) => {
+    res.type("text/plain")
+    res.status(400)
+    res.send("404 - not found")
+})
+
+// 500 page
+app.use((err, req, res, next) => {
+    console.log(err.message)
+    res.type("text/plain")
+    res.status(500)
+    res.send("500 - server error")
+})
+
+app.listen(port, () => console.log(
+    `express started on port ${port}`
+))
